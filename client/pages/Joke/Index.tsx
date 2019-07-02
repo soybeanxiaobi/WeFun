a89d4b907cdb33185a6fd469b1167a4727607c6d
chenxiaobin <chenxiaobin@youzan.com>
2019-07-02 17:39:57 +0800

    [feat]1.新增loading状态 2.新增error处理

--- client/pages/Joke/Index.tsx
+++ client/pages/Joke/Index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Card } from 'zent';
+import { Card, Loading, Notify } from 'zent';
 
 import { fetchJokeList } from '../../services/Joke';
 import './index.scss';
@@ -11,19 +11,28 @@ interface IJokeList {
 
 export const Joke = () => {
   const [jokeList, setJokeList] = useState([]);
+  const [loading, setIsLoading] = useState(false);
+
   useEffect(() => {
     const asyncFetch = async () => {
-      await fetchJokeList().then(result => {
+      setIsLoading(true);
+      try {
+        const result = await fetchJokeList();
         setJokeList(result.data);
-      });
+      } catch (err) {
+        Notify.error(err);
+      }
+      setIsLoading(false);
     }
     asyncFetch();
   }, []);
   return (
-    <div className="joke-wrapper">{jokeList.map((item: IJokeList) => (
-      <Card key={item.id} className="card-style">
-        <p>{item.content}</p>
-      </Card>
-    ))}</div>
+    <div className="joke-wrapper">{
+      loading ? <Loading show={true} /> : jokeList.map((item: IJokeList) => (
+        <Card key={item.id} className="card-style">
+          <p>{item.content}</p>
+        </Card>
+      ))}
+    </div>
   )
 }
