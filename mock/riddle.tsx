b18e6d4600c7ebb9bb84da9a8e7ee91e9fe94876
chenxiaobin <chenxiaobin@youzan.com>
2019-07-03 15:49:44 +0800

    [feat]新增riddle mock数据

--- mock/riddle.tsx
+++ mock/riddle.tsx
@@ -2,6 +2,8 @@ const RIDDLE = {
   'GET /riddle/list': [
     { id: 1, title: 'guess', question: '哪个地方不能讲笑话?', answer: '海边。因为引发海笑(啸)' },
     { id: 2, title: 'guess', question: '台风天带多少钱出门合适?', answer: '三千万。因为没伞(三)千万不要出门' },
+    { id: 3, title: 'guess', question: '一个猎人向一只狐狸开枪 为什么猎人死了?', answer: '反射弧' },
+    { id: 4, title: 'guess', question: '为什么女娲一边捏泥人一边笑?', answer: '因为做人呐,最重要的就是开心' },
   ]
 }
 module.exports = RIDDLE;
