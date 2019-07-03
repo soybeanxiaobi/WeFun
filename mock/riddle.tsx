eec586eabf301e8f6af9b877533e6daca3f8c03b
chenxiaobin <chenxiaobin@youzan.com>
2019-07-03 15:52:14 +0800

    [feat]新增riddle mock数据

--- mock/riddle.tsx
+++ mock/riddle.tsx
@@ -4,6 +4,8 @@ const RIDDLE = {
     { id: 2, title: 'guess', question: '台风天带多少钱出门合适?', answer: '三千万。因为没伞(三)千万不要出门' },
     { id: 3, title: 'guess', question: '一个猎人向一只狐狸开枪 为什么猎人死了?', answer: '反射弧' },
     { id: 4, title: 'guess', question: '为什么女娲一边捏泥人一边笑?', answer: '因为做人呐,最重要的就是开心' },
+    { id: 5, title: 'guess', question: '一头小猪一百块，为什么两头小猪要一千块?', answer: '两个头小猪那么珍贵，肯定卖得贵' },
+    { id: 6, title: 'guess', question: '你为什么单身?', answer: '因为大家都觉得像我这样的人不可能是单身,然后就都不考虑我了'}
   ]
 }
 module.exports = RIDDLE;
